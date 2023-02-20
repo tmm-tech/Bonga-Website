e22651d37d98db0494b35d0cc57a8de92465f37d
williammunyakathuo <tjwellieyam@gmail.com>
2023-02-20 12:53:39 +0300

    williams commit

---
+++ bonga/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Leftnav from './components/leftnav';
+import Rightnav from './components/rightnav';
+
+function App() {
+  return (
+    <div className="App">
+      <Leftnav/>
+      <Rightnav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bonga/src/components/rightnav.jsx
@@ -0,0 +1,33 @@
+const Rightnav = () => {
+    return (
+        <div className="rightnav">
+            <div className="part4">
+                <h3>YOUR PAGE</h3>
+                <div className="profiledetails">
+                    <img src="" alt="profilepi" srcset="" />
+                    <div className="details">
+                        <h4>mypage</h4>
+                        <p>Messages</p>
+                        <p>Notifications</p>
+                    </div>
+                </div>
+                <div className="options">
+                    <div className="likes">
+                        LIKES
+                    </div>
+                    <div className="VIEWS">
+                        VIEWS
+                    </div>
+                </div>
+                <div className="numbers">
+                    889 views
+                </div>
+            </div>
+            <div className="part5">
+                <h3>PROFILE INTRO</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Rightnav;
