884448494ec4e79e64b4ad0bec313f568165a8a2
williammunyakathuo <tjwellieyam@gmail.com>
2023-02-20 16:01:32 +0300

    side nav bars commits

--- bonga/src/App.js
+++ bonga/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
-import Leftnav from './components/leftnav';
-import Rightnav from './components/rightnav';
+import Leftnav from './components/Leftnav';
+import Rightnav from './components/Rightnav';
+import Navbar from './components/NavBar';
+import PostDisplay from './components/PostDisplay';
+import Footer from './components/Footer';
+import Login from ''
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Leftnav/>
+      <PostDisplay/>
+      <Login />
       <Rightnav/>
+      <Footer />
     </div>
   );
 }
